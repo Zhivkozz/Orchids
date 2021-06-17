8ae0a75e960fbcb1bcfae20524af06f243117105
Zhivkozz <48858768+Zhivkozz@users.noreply.github.com>
2021-06-17 23:43:44 +0300

    Update data.js
    Data

--- cypress/integration/orchidsDemo/testData/data.js
+++ cypress/integration/orchidsDemo/testData/data.js
@@ -6,7 +6,7 @@ module.exports = {
   newProducts: [
     {
       prName: "Test_product",
-      prCode: "1234Testzz",
+      prCode: "1234Testzz1",
       prDescription: "descriptionForTest",
     },
   ],
